cf1dd284efb12ca7cbd50ae19129b198726e9e1b
SerjPetersson <uefa90@gmail.com>
2022-04-26 15:17:02 +0600

    OK

--- src/basedclasses/response.py
+++ src/basedclasses/response.py
@@ -18,9 +18,9 @@ class Response:
 
     def assert_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status in status_code, self
+            assert self.response_status in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         else:
-            assert self.response_status == status_code, self
+            assert self.response_status == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
         return self
 
     def get_parsed_object(self):
